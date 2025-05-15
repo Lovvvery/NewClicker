f8c81c4d601825b661d3761bb46d349846e7557f
Lovvvery <itsdeslon2@gmail.com>
2025-05-16 03:44:33 +0700

    Добавил 2 кнопки в магазин и сделал покупку +50 за клик

--- app/src/main/java/com/example/myclicker/ShopActivity.kt
+++ app/src/main/java/com/example/myclicker/ShopActivity.kt
@@ -2,6 +2,7 @@ package com.example.myclicker
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -10,11 +11,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
 
 class ShopActivity : AppCompatActivity() {
     private lateinit var prefs: SharedPreferences
     private var money = 0
     private var moneyPlus = 10
+    private var moneyPlusPlus = 50
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +50,28 @@ class ShopActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "Недостаточно средств", Toast.LENGTH_SHORT).show()
             }
+        }
+        val btn2: Button = findViewById(R.id.buttonBuy2)
+
+        btn2.setOnClickListener {
+            var money = prefs.getInt(Constants.MONEY_KEY,0)
+            if(money >= 1000){
+                money -= 1000
+                moneyPlusPlus = 50
+                prefs.edit{
+                    putInt(Constants.MONEY_KEY,money)
+                    putInt(Constants.MONEY_PLUS_KEY,50)
+                }
+                Toast.makeText(this,"Покупка успешна!", Toast.LENGTH_SHORT).show()
+            }
+            else
+                Toast.makeText(this,"Недостаточно средств", Toast.LENGTH_SHORT).show()
+        }
+        var btn3: Button = findViewById(R.id.buttonBuy4)
+
+        btn3.setOnClickListener {
+            var money = prefs.getInt(Constants.MONEY_KEY,0)
+
         }
     }
 
